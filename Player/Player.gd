e07ee4185725b288cba88bd3b648c3d878434653
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2024-04-12 16:31:44 +0800

    Add idle animation function

--- Player/Player.gd
+++ Player/Player.gd
@@ -7,6 +7,10 @@ const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var anim = get_node("AnimatedSprite2D")
+
+func _ready():
+	anim.play("Idle")
 
 func _physics_process(delta):
 	# Add the gravity.
