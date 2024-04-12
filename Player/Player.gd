833fe9299930234e5077ab51b1ad9a3af650dac8
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2024-04-12 16:38:07 +0800

    Add Run and Idle functions

--- Player/Player.gd
+++ Player/Player.gd
@@ -26,7 +26,9 @@ func _physics_process(delta):
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		anim.play("Run")
 	else:
+		anim.play("Idle")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
