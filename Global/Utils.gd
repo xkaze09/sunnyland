f1d6ac1ee60f5d12d974c5143bc78d0c434bca73
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2024-04-13 16:49:28 +0800

    Add load function

--- Global/Utils.gd
+++ Global/Utils.gd
@@ -14,3 +14,9 @@ func saveGame():
 	
 func loadGame():
 	var file = FileAccess.open(SAVE_PATH, FileAccess.READ)
+	if FileAccess.file_exists(SAVE_PATH) == true:
+		if not file.eof_reached():
+			var current_line = JSON.parse_string(file.get_line())
+			if current_line:
+				Game.playerHP = current_line["playerHP"]
+				Game.gold = current_line["gold"]
--- main.gd
+++ main.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 func _ready():
 	Utils.saveGame()
+	Utils.loadGame()
 
 func _on_button_2_pressed():
 	get_tree().quit()
