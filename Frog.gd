83ca0ace7082e9f5c2fa7f4efd150c0e551046db
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2024-04-13 14:38:46 +0800

    Determine which animation to play

--- Frog.gd
+++ Frog.gd
@@ -11,7 +11,9 @@ func _physics_process(delta):
 	# Add the gravity.
 	velocity.y += gravity * delta
 	if chase == true:
-		get_node("AnimatedSprite2D").play("Jump")
+		if get_node("AnimatedSprite2D").animation != "Death":	
+			get_node("AnimatedSprite2D").play("Jump")
+			
 		player = get_node("../../Player/Player")
 		var direction = (player.position - self.position).normalized()
 		if direction.x > 0:
@@ -20,7 +22,8 @@ func _physics_process(delta):
 			get_node("AnimatedSprite2D").flip_h = false
 		velocity.x = direction.x * SPEED
 	else:
-		get_node("AnimatedSprite2D").play("Idle")
+		if get_node("AnimatedSprite2D").animation != "Death":	
+			get_node("AnimatedSprite2D").play("Idle")
 		velocity.x = 0
 	move_and_slide()
 	
@@ -35,4 +38,6 @@ func _on_player_detection_body_exited(body):
 
 func _on_player_death_body_entered(body):
 	if body.name == "Player":
+		get_node("AnimatedSprite2D").play("Death")
+		await get_node("AnimatedSprite2D").animation_finished
 		self.queue_free()
