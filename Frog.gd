913dd527bca66454311f8db877c6ed36bd20814d
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2024-04-13 11:58:31 +0800

    Add gravity to Frog

--- Frog.gd
+++ Frog.gd
@@ -1,7 +1,15 @@
 extends CharacterBody2D
 
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
 
+	move_and_slide()
+	
 func _on_player_detection_body_entered(body):
 	if body.name == "Player":
 		print("Player") # Replace with function body.
