88d9122ac1b3af381ea8c4e6f8bc121e13d7b81f
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2024-04-13 16:39:04 +0800

    Add save and load game functions

--- Global/Utils.gd
+++ Global/Utils.gd
@@ -1,3 +1,10 @@
 extends Node
 
 
+const SAVE_PATH = "res://savegame.bin"
+
+func saveGame():
+	var file = FileAccess.open(SAVE_PATH, FileAccess.WRITE)
+	
+func loadGame():
+	var file = FileAccess.open(SAVE_PATH, FileAccess.READ)
