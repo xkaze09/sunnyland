5b82990aa540f7988cc21da8eace7d6fc224d4ce
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2024-04-13 14:19:36 +0800

    Update logic

--- Frog.gd
+++ Frog.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+var SPEED = 50
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var player
@@ -9,15 +10,21 @@ var chase = false
 func _physics_process(delta):
 	# Add the gravity.
 	velocity.y += gravity * delta
-	player = get_node("../../Player/Player")
-	var direction = (player.position - self.position).normalized()
-	if direction.x > 0:
-		print("Right")
-	else:
-		print("Left")
+	if chase == true:
+		player = get_node("../../Player/Player")
+		var direction = (player.position - self.position).normalized()
+		if direction.x > 0:
+			get_node("AnimatedSprite2D").flip_h = true
+		else:
+			get_node("AnimatedSprite2D").flip_h = false
+		velocity.x = direction.x * SPEED
 		
 	move_and_slide()
 	
 func _on_player_detection_body_entered(body):
 	if body.name == "Player":
 		chase = true
+
+func _on_player_detection_body_exited(body):
+	if body.name == "Player":
+		chase = false
