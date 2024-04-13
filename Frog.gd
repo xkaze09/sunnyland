62ce92476e26e084f34fb1a2d4626c4039d18b6c
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2024-04-13 14:49:12 +0800

    Remove redundant velocity removal

--- Frog.gd
+++ Frog.gd
@@ -41,7 +41,6 @@ func _on_player_detection_body_exited(body):
 
 func _on_player_death_body_entered(body):
 	if body.name == "Player":
-		velocity.x = 0
 		chase = false
 		get_node("AnimatedSprite2D").play("Death")
 		await get_node("AnimatedSprite2D").animation_finished
