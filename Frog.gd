931484fc8c58db4ef411e7a46e569db33462fdb0
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2024-04-13 14:07:22 +0800

    Refactor

--- Frog.gd
+++ Frog.gd
@@ -3,18 +3,21 @@ extends CharacterBody2D
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var player
+var chase = false
+
+
 func _physics_process(delta):
 	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
+	velocity.y += gravity * delta
+	player = get_node("../../Player/Player")
+	var direction = (player.position - self.position).normalized()
+	if direction.x > 0:
+		print("Right")
+	else:
+		print("Left")
+		
 	move_and_slide()
 	
 func _on_player_detection_body_entered(body):
 	if body.name == "Player":
-		player = get_node("../../Player/Player")
-		var direction = (player.position - self.position).normalized()
-		if direction.x > 0:
-			print("Right")
-		else:
-			print("Left")
+		chase = true
